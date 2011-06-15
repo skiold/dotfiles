710f980860c98ba70a81b289b439da14a70498b0
Adrian Silva <adrian@orendana.local>
2011-06-15 10:49:50 +0200

    prepare local sup filters

--- .sup/hooks/before-add-message.rb
+++ .sup/hooks/before-add-message.rb
@@ -3,25 +3,8 @@
 if message.list_address != nil
   message.add_label "list"
 end
-{"agile-spain" => "List-ID: <agile-spain.googlegroups.com>",
- "curvecp" => "Mailing-List: contact curvecp-help@list.cr.yp.to; run by ezmlm",
- "exherbo-dev" => "List-Id: <exherbo-dev.lists.exherbo.org>",
- "linux-um" => "List-Id: <linux\.listas\.um\.es>",
- "lonely-coaches" => "<lonely-coaches-sodality.googlegroups.com>",
- "madrid-pm" => 'List-Id: Lista de correo de Madrid Perl Mongers <madrid-pm\.pm\.org>',
- "madrid-devops" => "List-ID: <madrid-devops\.googlegroups\.com>",
- "mcollective-users" => "List-ID: <mcollective-users.googlegroups.com>",
- "peertransfer" => "Delivered-To: peertransfer@ageda.net",
- "puppet-dev" => "List-ID: <puppet-dev\.googlegroups\.com>",
- "puppet-users" => "List-ID: <puppet-users\.googlegroups\.com>",
- "chef" => "List-Id: <chef\.lists\.opscode\.com>",
- "spain-hadoop-users" => "List-ID: <spain-scalability-users\.googlegroups\.com>",
- "twisted-web" => "List-Id: Twisted Web World <twisted-web\.twistedmatrix\.com>",
- "twisted-python" => "List-Id: Twisted general discussion <twisted-python\.twistedmatrix\.com>",
- "babushka_app" => "List-ID: <babushka_app\.googlegroups\.com>",
- "soag" => "List-ID: <spain-openstack-user-group\.googlegroups\.com>",
- "bufferbloat" => "<bloat-devel\.lists\.bufferbloat\.net>",
- }.each_pair do |list_label, list_regex| 
+{"list" => "List-ID: ",
+}.each_pair do |list_label, list_regex| 
   if message.raw_header =~ /#{list_regex}/i
     message.add_label "list"
     message.add_label "#{list_label}"  
