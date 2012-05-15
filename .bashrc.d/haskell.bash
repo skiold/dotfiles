22d92b39261a71d159a437906b8bc4c34fefe00b
Adrian Silva <adrian@peertransfer.com>
2012-05-15 20:12:24 +0200

    use things register functions with rbenv & haskell
    so that they are idempotent.
    
    I need to extract the register functions out of main bashrc
    as they are required on places other than the login shell
    (i.e. .xinitrc, again)

--- .bashrc.d/haskell.bash
+++ .bashrc.d/haskell.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 haskell_up() {
-  export PATH="${HOME}/.cabal/bin:/opt/haskell/bin:${PATH}:/opt/local/libexec/gnubin"
+  if echo "$THINGS_UP" | grep -qv "Haskell: $(ghc --version)" ; then
+    export PATH="${HOME}/.cabal/bin:/opt/haskell/bin:${PATH}:/opt/local/libexec/gnubin"
+    add_things_up "Haskell: $(ghc --version)"
+  fi
 }
--- .bashrc.d/ruby-envs.bash
+++ .bashrc.d/ruby-envs.bash
@@ -1,9 +1,15 @@
 
 rvm_up() {
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-  [[ -s "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
+  if echo "$THINGS_UP" | grep -qv "Rvm" ; then
+    [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+    [[ -s "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
+    add_things_up "Rvm"
+  fi
 }
 
 rbenv_up() {
-  [[ -s "$HOME/.rbenv/bin" ]] && export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
+  if echo "$THINS_UP" | grep -qv "Rbenv: $(rbenv 2>&1 | head -n 1)" ; then
+    [[ -s "$HOME/.rbenv/bin" ]] && export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
+    add_things_up "Rbenv: $(rbenv 2>&1 | head -n 1)"
+  fi
 }
